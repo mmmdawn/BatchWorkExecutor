37f14b2a0f9ac1cd812296567c012a8d45fd940d
dawn <congminh292k@gmail.com>
2022-11-22 23:32:41 +0700

    update test

--- src/__tests__/runnable.test.ts
+++ src/__tests__/runnable.test.ts
@@ -6,7 +6,7 @@ for (let i = 1; i <= 1000; i++) {
     numbers.push(i)
 }
 
-const executor = new BatchWorkExecutor(50, 4)
+const executor = new BatchWorkExecutor(50, 4, {})
 
 const expectedResult = JSON.stringify([
     1275, 3775, 6275, 8775, 11275, 13775, 16275, 18775, 21275, 23775, 26275,
